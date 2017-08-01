25b2abb93dca1bfb36e00d73066e0733d23d2aa7
PraktikumGruppe VHDLP22 <vhdlp22@einsilbix.ies.e-technik.tu-darmstadt.de>
2017-08-01 09:16:18 +0200

    Some fixing

--- HDL_Lab_2017/HDL_Lab/sources/rtl/register_file_tb.v
+++ HDL_Lab_2017/HDL_Lab/sources/rtl/register_file_tb.v
@@ -1,5 +1,20 @@
+`define R0 4'b0000
+`define R1 4'b0001
+`define R2 4'b0010
+`define R3 4'b0011
+`define R4 4'b0100
+`define R5 4'b0101
+`define R6 4'b0110
+`define R7 4'b0111
+
+`define SP 4'b1000
+`define PC 4'b1001
+`define LR 4'b1010
+`define IMM 4'b1111
+
 module register_file_tb ();
 
 
 // some test cases
+wire 
 endmodule
