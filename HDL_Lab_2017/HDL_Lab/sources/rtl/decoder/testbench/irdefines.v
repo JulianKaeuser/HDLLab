a7dd38a97953610fa7f21f193957e442a8fd5425
Julian <jkaeus@googlemail.com>
2017-08-08 10:25:42 +0000

    testbnch for push, pop

---
+++ HDL_Lab_2017/HDL_Lab/sources/rtl/decoder/testbench/irdefines.v
@@ -0,0 +1,283 @@
+
+`define        ADDRESS_WIDTH    16
+
+
+// ALU-Opcodes                  // result = a OP b  // rd = op a, rs = op b
+`define        AND        5'b00000  // rd = rd AND rs
+`define        EOR        5'b00001  // rd = rd XOR rs
+`define        LSL        5'b00010  // rd = rd << rs
+`define        LSR        5'b00011  // rd = rd >> rs
+`define        ASR        5'b00100  // rd = rd ASR rs
+`define        ADC        5'b00101  // rd = rd + rs + c-bit
+`define        SBC        5'b00110  // rd = rd - rs - NOT c-bit
+`define        ROR        5'b00111  // rd = rd RROR rs
+`define        TST        5'b01000  // set condition codes i´on rd AND rs
+`define        NEG        5'b01001  // rd = ~rs
+`define        CMP        5'b01010  // set condition codes on rd - rs
+`define        CMN        5'b01011  // set condition codes on rd + rs
+`define        ORR        5'b01100  // rd = rd OR rs
+`define        MUL        5'b01101  // rd = rs * rd  // Reihenfolge vertauscht!!
+`define        BIC        5'b01110  // rd = rd AND NOT rs
+`define        MVN        5'b01111  // rd = NOT rs
+
+`define        ADD        5'b10101  // rd = rd + rs
+`define        SUB        5'b10110  // rd = rd - rs
+
+// Datenbreiten
+`define        BYTE       2'b00  
+`define        HALFWORD   2'b01  
+`define        WORD       2'b10
+`define        ILLEGAL    2'b11
+
+// Registerfile
+
+`define       RF_R0        5'b00000
+`define       RF_R1        5'b00001
+`define       RF_R2        5'b00010
+`define       RF_R3        5'b00011
+`define       RF_R4        5'b00100
+`define       RF_R5        5'b00101
+`define       RF_R6        5'b00110
+`define       RF_R7        5'b00111
+`define       RF_R8        5'b01000
+`define       RF_R9        5'b01001
+`define       RF_R10       5'b01010
+`define       RF_R11       5'b01011
+`define       RF_R12       5'b01100
+`define       RF_R13_SP    5'b01101
+`define       RF_R14_LR    5'b01110
+`define       RF_R15_PC    5'b01111
+
+`define       RF_NONE      5'b11101  // für synthese auf 5'b????? ändern
+`define       RF_TMP1      5'b11110
+`define       RF_IMM       5'b11111
+
+// sonstiges
+
+`define       IMM_ZERO    32'b0000_0000_0000_0000_0000_0000_0000_0000
+`define       IMM_ONES    32'b1111_1111_1111_1111_1111_1111_1111_1111
+
+`define       DONE        1'b1
+`define       NOT_DONE    1'b0
+
+
+//Reihenfolge
+// input wire clock;
+// input wire [15:0] instruction;
+// 
+// input wire flag_z;
+// input wire flag_c;
+// input wire flag_n;
+// input wire flag_v;
+// 
+// input wire stall_from_instructionfetch; 
+// input wire stall_from_memorycontroller;
+// 
+// output reg  [4:0] operand_a;           // für 8-fach Registerfile Bit 3 ignorieren: {operand_a[4], operand_a[3:0]}
+// output reg  [4:0] operand_b;
+// 
+// output reg [31:0] offset_a;
+// output reg [31:0] offset_b;
+// 
+// output reg  [3:0] alu_opcode;
+// 
+// output reg update_flag_n;
+// output reg update_flag_z;
+// output reg update_flag_c;
+// output reg update_flag_v;
+// 
+// output reg  [4:0] alu_write_to_reg;
+// output reg alu_write_to_reg_enable; 
+// output reg  [4:0] memory_write_to_reg;
+// output reg memory_write_to_reg_enable;
+// 
+// output reg  [4:0] memory_store_data_reg;
+// output reg  [4:0] memory_store_address_reg;
+// output reg memory_address_source_is_reg;
+// output reg  [1:0] load_store_width;
+// output reg memorycontroller_sign_extend;
+// 
+// output reg memory_load_request;
+// output reg memory_store_request;
+// 
+// output reg stack_push_request;
+// output reg stack_pop_request;
+// 
+// output reg stall_to_instructionfetch;
+// 
+// output wire decoder_pc_update;
+// 
+// reg [7:0] step;                      // default all 1 !
+
+
+// ARM-Thumb-Opcodes
+
+// Format 1 und 2
+`define       FORMAT_1_2          16'b000?_????_????_????  // 5.1, 5.2
+
+`define       F1_LSL       2'b00      
+`define       F1_LSR       2'b01
+`define       F1_ASR       2'b10
+`define       FORMAT_2     2'b11
+
+`define       F2_ADD       1'b0      
+`define       F2_SUB       1'b1
+
+`define       F2_REG_OP    1'b0      
+`define       F2_IMM_OP    1'b1
+
+// Format 3
+`define       FORMAT_3          16'b001?_????_????_????  // 5.3
+
+`define       F3_MOV       2'b00      
+`define       F3_CMP       2'b01
+`define       F3_ADD       2'b10
+`define       F3_SUB       2'b11
+
+// Format 4
+`define       FORMAT_4          16'b0100_00??_????_????  // 5.4
+
+`define       F4_AND       4'b0000      
+`define       F4_EOR       4'b0001
+`define       F4_LSL       4'b0010
+`define       F4_LSR       4'b0011
+`define       F4_ASR       4'b0100      
+`define       F4_ADC       4'b0101
+`define       F4_SBC       4'b0110
+`define       F4_ROR       4'b0111
+`define       F4_TST       4'b1000      
+`define       F4_NEG       4'b1001
+`define       F4_CMP       4'b1010
+`define       F4_CMN       4'b1011
+`define       F4_ORR       4'b1100
+`define       F4_MUL       4'b1101
+`define       F4_BIC       4'b1110
+`define       F4_MVN       4'b1111
+
+// Format 5
+`define       FORMAT_5          16'b0100_01??_????_????  // 5.5   
+
+`define       F5_ADD       2'b00      
+`define       F5_CMP       2'b01
+`define       F5_MOV       2'b10
+`define       F5_BX        2'b11
+
+`define       F5_BX_RS     1'b0 
+`define       F5_BX_HS     1'b1 
+
+// Format 6
+`define       FORMAT_6          16'b0100_1???_????_????  // 5.6 
+
+// Format 7
+`define       FORMAT_7          16'b0101_??0?_????_????  // 5.7 
+
+`define       F7_STORE     1'b0
+`define       F7_LOAD      1'b1      
+
+`define       F7_WORD      1'b0      
+`define       F7_BYTE      1'b1
+
+// Format 8
+`define       FORMAT_8          16'b0101_??1?_????_????  // 5.8 
+
+`define       F8_STORE_HW      2'b00      
+`define       F8_LOAD_HW       2'b01
+`define       F8_LOAD_SE_B     2'b10
+`define       F8_LOAD_SE_HW    2'b11
+
+// Format 9
+`define       FORMAT_9          16'b011?_????_????_????  // 5.9
+
+`define       F9_STORE     1'b0
+`define       F9_LOAD      1'b1 
+
+`define       F9_WORD      1'b0 
+`define       F9_BYTE      1'b1
+
+//Format 10
+`define       FORMAT_10         16'b1000_????_????_????  // 5.10
+
+`define       F10_STORE     1'b0
+`define       F10_LOAD      1'b1
+
+// Format 11
+`define       FORMAT_11         16'b1001_????_????_????  // 5.11
+
+`define       F11_STORE     1'b0
+`define       F11_LOAD      1'b1
+
+// Format 12
+`define       FORMAT_12         16'b1010_????_????_????  // 5.12
+
+`define       F12_PC        1'b0
+`define       F12_SP        1'b1
+
+
+// Format 13
+`define       FORMAT_13         16'b1011_0000_????_????  // 5.13
+
+`define       F12_ADD        1'b0
+`define       F12_SUB        1'b1
+
+// Format 14
+`define       FORMAT_14         16'b1011_?10?_????_????  // 5.14
+
+`define       F14_PUSH     1'b0
+`define       F14_POP      1'b1
+
+// Format 15
+`define       FORMAT_15         16'b1100_????_????_????  // 5.15
+
+`define       F15_STORE     1'b0
+`define       F15_LOAD      1'b1
+
+// Format 16
+`define       FORMAT_16_17      16'b1101_????_????_????  // 5.16 / 5.17
+
+`define       F16_BEQ       4'b0000      
+`define       F16_BNE       4'b0001
+`define       F16_BCS       4'b0010
+`define       F16_BCC       4'b0011
+`define       F16_BMI       4'b0100      
+`define       F16_BPL       4'b0101
+`define       F16_BVS       4'b0110
+`define       F16_BVC       4'b0111
+`define       F16_BHI       4'b1000      
+`define       F16_BLS       4'b1001
+`define       F16_BGE       4'b1010
+`define       F16_BLT       4'b1011
+`define       F16_BGT       4'b1100
+`define       F16_BLE       4'b1101
+`define       F16_ILLEGAL   4'b1110  // undefined
+`define       F17_SWI       4'b1111  // SW-Interrupt, not implemented
+
+// Format 18
+`define       FORMAT_18         16'b1110_0???_????_????  // 5.18
+
+// Format 19
+`define       FORMAT_19         16'b1111_????_????_????  // 5.19
+
+`define       F19_HIGH     1'b0
+`define       F19_LOW      1'b1
+
+// Format 20 // IT-Instruction    // Seite 416
+`define       FORMAT_20         16'b1011_1111_????_????  
+
+
+   
+
+
+
+   
+
+
+
+   
+
+
+
+
+
+
+
+ 
---
+++ HDL_Lab_2017/HDL_Lab/sources/rtl/decoder/testbench/testbench6.sv
@@ -0,0 +1,137 @@
+// Integrated Electronic Systems Lab
+// TU Darmstadt
+// Author:	Dipl.-Ing. Boris Traskov
+// Modified by M.Sc. Haoyuan Ying Dual 8-bit Memory Port -> Single 16-bit
+// Email: 	boris.traskov@ies.tu-darmstadt.de
+
+`timescale 1 ns / 1 ps
+
+module testbench6();
+
+// PARAMETERS
+parameter MEM_DEPTH   		= 2**12;	//8192 Bytes 4096*2B
+parameter ADDR_WIDTH   		= $clog2(MEM_DEPTH);
+parameter string filename	= "mem_unititialized.bin";
+
+// ================
+// Internal Signals
+// ================
+
+integer file, status; // needed for file-io
+
+logic			clock;
+logic			reset;
+
+// ================
+// Memory Interface
+// ================
+
+logic  [15:0] MEM_MEMCTRL_from_mem_data;
+
+logic         MEMCTRL_MEM_to_mem_read_enable   ;
+logic         MEMCTRL_MEM_to_mem_write_enable  ;
+logic         MEMCTRL_MEM_to_mem_mem_enable    ;
+logic  [11:0] MEMCTRL_MEM_to_mem_address       ;
+logic  [15:0] MEMCTRL_MEM_to_mem_data          ;
+
+// ======
+// Inputs
+// ======
+// logic          clock;
+// logic          reset;
+logic          [15:0] instruction;
+
+logic          stall_from_instructionfetch;
+
+// =======
+// Outputs
+// =======
+
+logic          stall_to_instructionfetch;
+
+logic          decoder_pc_update;
+
+logic          [31:0] RF_pc_out;
+
+
+
+
+
+// =================
+// CPU Instantiation
+// =================
+top5 UUT (
+
+    /* input */          .clock                              ( clock                               ),
+    /* input */          .reset                              ( reset                               ),
+    /* input */          .instruction                        ( instruction                         ),
+
+    /* input */          .stall_from_instructionfetch        ( stall_from_instructionfetch         ),
+
+    /* output */         .stall_to_instructionfetch          ( stall_to_instructionfetch           ),
+
+    /* output */         .decoder_pc_update                  ( decoder_pc_update                   ),
+
+    /* output */         .RF_pc_out                          ( RF_pc_out                           ),
+
+    // ***************************************************
+
+    /* input */          .MEM_MEMCTRL_from_mem_data          ( MEM_MEMCTRL_from_mem_data           ),
+
+    /* output */         .MEMCTRL_MEM_to_mem_read_enable     ( MEMCTRL_MEM_to_mem_read_enable      ),
+    /* output */         .MEMCTRL_MEM_to_mem_write_enable    ( MEMCTRL_MEM_to_mem_write_enable     ),
+    /* output */         .MEMCTRL_MEM_to_mem_mem_enable      ( MEMCTRL_MEM_to_mem_mem_enable       ),
+    /* output */         .MEMCTRL_MEM_to_mem_address         ( MEMCTRL_MEM_to_mem_address          ),
+    /* output */         .MEMCTRL_MEM_to_mem_data            ( MEMCTRL_MEM_to_mem_data             )
+
+
+);
+
+
+
+
+
+//  MEMORY MODULE INSTANTIATION
+memory #(
+	.MEM_DEPTH (MEM_DEPTH))
+memory_i (
+   	.clk 	(clock),
+    .addr	(MEMCTRL_MEM_to_mem_address),
+    .en  	(MEMCTRL_MEM_to_mem_mem_enable),
+    .rd_en  (MEMCTRL_MEM_to_mem_read_enable),
+    .wr_en  (MEMCTRL_MEM_to_mem_write_enable),
+    .din (MEMCTRL_MEM_to_mem_data),
+    .dout(MEM_MEMCTRL_from_mem_data)
+    );
+
+// CLOCK GENERATOR
+initial begin
+	clock = 1'b0;
+	forever #1  clock = !clock;
+end
+
+// RESET GENERATOR
+initial begin
+	reset			= 1'b0;
+	file		= $fopen(filename, "r");
+	#3 reset		= 1'b1;     // 3   ns
+	status		= $fread(memory_i.ram, file);
+	#2.1 reset	= 1'b0;  //2.1 ns
+//	$finish;
+end
+
+// STIMULI
+initial begin
+
+instruction                     = 16'h0000;
+
+stall_from_instructionfetch     = 1'b0;
+
+
+
+
+end
+
+
+
+endmodule
