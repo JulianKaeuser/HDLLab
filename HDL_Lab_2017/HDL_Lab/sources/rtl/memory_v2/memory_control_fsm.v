98af2b9e68850795e41df75b417df487787ae4b0
Julian <jkaeus@googlemail.com>
2017-08-10 09:32:23 +0000

    added memory control fsm v2

---
+++ HDL_Lab_2017/HDL_Lab/sources/rtl/memory_v2/memory_control_fsm.v
@@ -0,0 +1,21 @@
+// AUTHORS Group 06 /Julian Käuser
+// Thursday, 08/10/2017
+
+module memory_control_fsm (
+  clk,
+  reset,
+  busy,
+  output_valid,
+  write_ready,
+  fsm_rd,
+  fsm_wr,
+  fsm_wr_en,
+  fsm_rd_en,
+  fsm_mem_en,
+  
+  );
+
+
+
+
+endmodule
